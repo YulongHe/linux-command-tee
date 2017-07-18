28fb78b0c6d420b14bf570b62cfe80fd614619a7
Yulong.He <hylong2111@163.com>
2017-07-18 10:42:09 +0800

    Update tee.c

---
+++ tee.c
@@ -0,0 +1,17 @@
+/**
+* @brief: this copy.c used to copy file.realize copy(1)
+*
+* @param: oldfile newfile(which file u want to copy)
+*
+* @return: 0 is success
+*
+* @log: yulong -20170711 first edit
+**/
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[])
+{
+
+}       
