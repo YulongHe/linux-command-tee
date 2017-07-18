1c692668f7d1f934ba295179db8a935d148275bb
YulongHe <hylong2111@163.com>
2017-07-18 11:22:03 +0800

    longge test

--- tee.c
+++ tee.c
@@ -1,17 +1,21 @@
 /**
-* @brief: this copy.c used to copy file.realize copy(1)
+* @brief: this tee.c used general open/write/close. realize tee(1)
 *
-* @param: oldfile newfile(which file u want to copy)
+* @param: the file(if not exixt) you want write(use general input)
 *
 * @return: 0 is success
 *
-* @log: yulong -20170711 first edit
+* @log: yulong -20170718 first edit
 **/
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<unistd.h>
 
+#define buffer_size 1024
 int main(int argc, char *argv[])
-{
+{	
+    int InputFd, OutputFd;
 
+//	if(argc == )
+	printf("%d, %s\n", argc, argv[0]);
 }       
